065b31fece5f1397f44dd607a06ebd2b1254adec
Archit Mahajan <mahajanarchit83@gmail.com>
2026-09-20 15:02:12 +0530

    Add deposit, withdraw, and transaction history methods

--- src/main/java/com/bank/bankmanagement/controller/AccountController.java
+++ src/main/java/com/bank/bankmanagement/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.bank.bankmanagement.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.bankmanagement.model.Account;
+import com.bank.bankmanagement.model.Transaction;
 import com.bank.bankmanagement.service.AccountService;
 
 @RestController
@@ -37,5 +39,24 @@ public class AccountController {
     @GetMapping
     public List<Account> getAll() {
         return accountService.getAllAccounts();
+    }
+
+    @PostMapping("/deposit")
+    public String deposit(@RequestParam int accountNo,
+                           @RequestParam double amount) {
+        boolean ok = accountService.deposit(accountNo, amount);
+        return ok ? "Deposit successful" : "Deposit failed";
+    }
+
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestParam int accountNo,
+                            @RequestParam double amount) {
+        boolean ok = accountService.withdraw(accountNo, amount);
+        return ok ? "Withdrawal successful" : "Withdrawal failed - insufficient balance or invalid account";
+    }
+
+    @GetMapping("/transactions/{accountNo}")
+    public List<Transaction> getTransactionHistory(@PathVariable int accountNo) {
+        return accountService.getTransactionHistory(accountNo);
     }
 }
